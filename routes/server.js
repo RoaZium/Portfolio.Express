150df223bdd187c957fd42f03c8a3d76b5c86576
황진욱 <40532902+RoaZium@users.noreply.github.com>
2022-04-13 11:07:46 +0900

    API 연동 작업

--- routes/server.js
+++ routes/server.js
@@ -1,18 +1,73 @@
-//server.js
+// import axios from 'axios';
+const axios = require('axios');
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const app = express();
+const request = require('request');
 
 const server = require('http').createServer(app);
 
 app.use(cors()); // cors 미들웨어를 삽입합니다.
 
-app.get('/', (req,res) => { // 요청패스에 대한 콜백함수를 넣어줍니다.
-  res.send({message:'hellow'});
+app.get('/admin', (req, res, next) => { // 요청패스에 대한 콜백함수를 넣어줍니다.
+  res.send({
+    message: 'hellow HJW'
+  });
+  console.log('server Call')
+})
+
+/* axios.get('/', (req, res, next) => { // 요청패스에 대한 콜백함수를 넣어줍니다.
+  res.send({
+    message: 'hellow HJW'
+  });
+  console.log('server Call')
+}); */
+
+/* const instance = axios.create({
+  baseURL: 'http://20.194.27.202:48090/v1/visitormanager?visitor_name=김민구&site_id=01f0ea04-6040-47d8-a756-f1b08d855096',
+  timeout: 1000,
+  headers: {'login_token': '98A047DF-0C2D-402D-8397-86AC011D09A8'}
+}) */
+
+/* app.get('/', (req,res) => { // 요청패스에 대한 콜백함수를 넣어줍니다.
+  res.send({message:'hellow HJW'});
+}); */
+
+app.get('/', function (req, res) {
+  // req.headers['login_token'] = '98A047DF-0C2D-402D-8397-86AC011D09A8'
+  request('https://api.publicapis.org/entries', function (error, response, body) {
+    res.send(body)
+  })
+})
+
+var options = {
+  'method': 'GET',
+  'url': 'https://api.publicapis.org/entries',
+  'headers': {
+  }
+};
+
+app.get('/a', (req, res, next) => { // 요청패스에 대한 콜백함수를 넣어줍니다.
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    console.log(response.body);
+
+    res.send(response.body)
+  })
+
+  res.send({
+    message: 'hellow HJW'
+  });
+  console.log('server Call')
 });
 
-server.listen(5000, ()=>{
+
+/* app.get('/', (req,res) => { // 요청패스에 대한 콜백함수를 넣어줍니다.
+  res.send({message:'hellow HJW'});
+}); */
+
+server.listen(5000, () => {
   console.log('server is running on 5000')
 })
 
